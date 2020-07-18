f6a2bcd2e3d0a0c8a777ead64896e27c7137e6d2
Max Kohler <max@koehler-kn.de>
2020-07-18 13:29:42 +0100

    Parse body

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('FGL email bot is running.')
 })
 
 app.post(`/`, (req, res) => {
-    console.log(req)
+    console.log(req.body)
 })
 
 app.listen(port, () => console.log(`Listening at port ${port}`))
