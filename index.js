274175991d6fa509bb43e896a3ead3ac5d16094d
Max Kohler <max@koehler-kn.de>
2020-07-18 15:25:24 +0100

    implement email sending

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const port = process.env.PORT || 3000
 
 const nodemailer = require('nodemailer')
 require('dotenv').config();
+
+
 const transporter = nodemailer.createTransport({
-    host: "smtp.fgl-konstanz.de",
+    host: "smtp.udag.de",
     port: 587,
     secure: false,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PW
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
+
 
 app.use(express.json());
 app.get('/', (req, res) => {
@@ -23,17 +29,18 @@ app.post(`/${process.env.TT_URL}`, (req, res) => {
     console.log("Received post request...")
     if (req.body.message) {
         console.log("Received message, attempting to send email...")
-        
-        const message = {
-            from: "bot@fgl-konstanz.de",
-            to: "nurmail@fgl-konstanz.de",
-            subject: "Telegram Bot",
+
+        var message = {
+            from: "max@koehler-kn.de",
+            to: process.env.TARGET_EMAIL,
+            subject: "FGL Telegram Bot",
             text: `${req.body.message.from.first_name} ${req.body.message.from.last_name} (${req.body.message.from.username}) schreibt: ${req.body.message.text}`
         };
 
         transporter.sendMail(message, (err, info) => {
             if (err) { console.log(err) }
         })
+
         res.send(true)
 
     } else {
